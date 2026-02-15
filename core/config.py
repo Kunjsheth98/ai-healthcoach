fc92af8d383c7eeace1aeb9cacefc5b179a66ed6
Kunjsheth98 <kunjsheth98@gmail.com>
2026-02-15 17:02:23 +0530

    removed api key

--- core/config.py
+++ core/config.py
@@ -1,6 +1,4 @@
+import os
 from openai import OpenAI
 
-# Central AI client (used everywhere)
-client = OpenAI(
-    api_key="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"
-)
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
